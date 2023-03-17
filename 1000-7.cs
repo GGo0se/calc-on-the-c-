9c5e5c6c461a4bb0e0255957118c13a12516513a
goose <77739225+GGo0se@users.noreply.github.com>
2023-03-17 23:40:10 +0500

    Add files via upload

---
+++ 1000-7.cs
@@ -0,0 +1,10 @@
+﻿int c = 1000; //initial number
+while (c > 0) //calculation cycle
+{
+    Thread.Sleep(001);
+    int i = c - 7;
+    Console.WriteLine("I`m Ghoule {0}", i);//write result THIS text
+    c = i;
+}
+Console.WriteLine("I`m Ghoule");// this text writen for fun
+
